f6307bdca26c141196d51e83698d6e6c0be253b5
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-08-12 10:18:25 -0600

    Initialize Todo-List Project

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+  entry: {
+    app: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -15,6 +12,11 @@ module.exports = {
       inject: "body",
     }),
   ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
   module: {
     rules: [
       {
