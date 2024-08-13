1a197f9f07a30216749b4dc786f8c3684fc2978f
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-08-12 21:34:23 -0600

    Set up basic structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 
 import "./style.css";
-
-let slideShow = true;
-
-const div = document.querySelector("#content");
-const btnHome = document.querySelector(".home");
-const btnMenu = document.querySelector(".menu");
-const btnContact = document.querySelector(".contact");
