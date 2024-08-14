e53ff77f01ca0aca969bf084a5cf31afd368fd8c
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-08-14 16:06:52 -0600

    Add different functionalities to the task UI

--- src/projectDisplay.js
+++ src/projectDisplay.js
@@ -7,7 +7,8 @@ export default function displayProject(projectsArray, dataProjectName) {
     if (element.projectName === dataProjectName) {
       mainContent.innerHTML = "";
       projectHeader.textContent = `${element.projectName}`;
-      projectHeader.style.cssText = "text-align: center; text-transform: capitalize;"
+      projectHeader.style.cssText =
+        "text-align: center; text-transform: capitalize;";
       projectDiv.appendChild(projectHeader);
       element.todoList.forEach((todo) => {
         const todoDiv = document.createElement("div");
@@ -22,24 +23,38 @@ export default function displayProject(projectsArray, dataProjectName) {
         dueDatePara.textContent = `${todo.dueDate}`;
 
         const priorityDiv = document.createElement("div");
-        if(todo.priority === "p1") {
-            priorityDiv.style.cssText = "width:50px; height:10px; background-color: rgb(200, 0, 0); border-radius: 7.5px;";
-        }
-        else if(todo.priority === "p2") {
-            priorityDiv.style.cssText = "width:50px; height:10px; background-color: rgb(255, 187, 0); border-radius: 7.5px;";
-        }
-        else if(todo.priority === "p3") {
-            priorityDiv.style.cssText = "width:50px; height:10px; background-color: rgb(0, 150, 0); border-radius: 7.5px;";
-        }
-        else {
-            priorityDiv.style.cssText = "width:50px; height:10px; background-color: gray; border-radius: 7.5px;";
+        if (todo.priority === "p1") {
+          priorityDiv.style.cssText =
+            "width:50px; height:10px; background-color: rgb(200, 0, 0); border-radius: 7.5px;";
+        } else if (todo.priority === "p2") {
+          priorityDiv.style.cssText =
+            "width:50px; height:10px; background-color: rgb(255, 187, 0); border-radius: 7.5px;";
+        } else if (todo.priority === "p3") {
+          priorityDiv.style.cssText =
+            "width:50px; height:10px; background-color: rgb(0, 150, 0); border-radius: 7.5px;";
+        } else {
+          priorityDiv.style.cssText =
+            "width:50px; height:10px; background-color: gray; border-radius: 7.5px;";
         }
 
-        todoDiv.append(titlePara, priorityDiv, dueDatePara, descriptionPara);
+        const todoFirstRow = document.createElement("div");
+        todoFirstRow.append(titlePara, priorityDiv);
+
+        const editTodoBtn = document.createElement("button");
+        editTodoBtn.classList.add("edit-todo-btn");
+
+        const deleteTodoBtn = document.createElement("button");
+        deleteTodoBtn.classList.add("delete-todo-btn");
+
+        const todoExtension = document.createElement("div");
+        todoExtension.append(descriptionPara, editTodoBtn, deleteTodoBtn);
+
+        todoDiv.append(todoFirstRow, dueDatePara, todoExtension);
         projectDiv.appendChild(todoDiv);
       });
       mainContent.appendChild(projectDiv);
-      projectDiv.style.cssText = "display: flex; flex-direction: column; gap: 1rem;"
+      projectDiv.style.cssText =
+        "display: flex; flex-direction: column; gap: 1rem;";
     }
   });
 }
